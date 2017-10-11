2f59759dac083ff13308bf28d8c24255c88a694f
ra <rayax86@gmail.com>
2017-10-11 13:58:05 +1100

    latest

--- main.c
+++ main.c
@@ -36,6 +36,7 @@
 #include "sfr_defs.h"
 #include <string.h>
 #include <stdlib.h>
+#include <jmorecfg.h>
 #include "macros.h"
 
 /*
@@ -187,30 +188,34 @@ void SC_init(void) {
 
 // MPU6050 Initial
 void init_MPU6050(void) {
-    char PW[] = {0x6B, 0x00};
+    byte PW[] = {0x6B, 0x00};
     MCU_SC_write(0b1101000, 2, PW);
-//  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
-//  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
-//  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
-//  Wire.endTransmission();
-    char GC[] = {0x1B, 0x00};
+
+    byte GC[] = {0x1B, 0x00};
     MCU_SC_write(0b1101000, 2, GC);
-//  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-//  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4)
-//  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s
-//  Wire.endTransmission();
-    char AC[] = {0x1C, 0x00};
+
+    byte AC[] = {0x1C, 0x00};
     MCU_SC_write(0b1101000, 2, AC);
-//  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-//  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5)
-//  Wire.write(0b00000000); //Setting the accel to +/- 2g
-//  Wire.endTransmission();
+
+    // The working example from Arudino:
+    //  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
+    //  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
+    //  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
+    //  Wire.endTransmission();
+    //  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+    //  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4)
+    //  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s
+    //  Wire.endTransmission();
+    //  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+    //  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5)
+    //  Wire.write(0b00000000); //Setting the accel to +/- 2g
+    //  Wire.endTransmission();
 }
 
-void recordAccelRegisters() {
+void MPU_6050_read() {
     uint8_t bytes_to_read = 6;
 
-    bool interrupt = ioport_get_pin_level(INT_SC);
+    boolean interrupt = ioport_get_pin_level(INT_SC);
     long accelX, accelY, accelZ;
     float gForceX, gForceY, gForceZ;
 
@@ -272,7 +277,7 @@ int main(void) {
         delay_ms(500);
         ioport_toggle_pin_level(LED);
         delay_ms(500);
-        recordAccelRegisters();
+        MPU_6050_read();
         delay_ms(500);
     }
 }
